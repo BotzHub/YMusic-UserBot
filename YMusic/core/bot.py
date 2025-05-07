1fd5740467b0339e0822a28702802acfb7803aad
Tech Shreyansh <165946497+TechyShreyansh@users.noreply.github.com>
2025-05-07 11:49:04 +0530

    Update bot.py

--- YMusic/core/bot.py
+++ YMusic/core/bot.py
@@ -1,7 +1,6 @@
 from pyrogram import Client
 from pytgcalls import PyTgCalls
 
-import platform
 import config
 from ..logging import LOGGER
 
@@ -9,22 +8,8 @@ api_id: int = config.API_ID
 api_hash: str = config.API_HASH
 session_string: str = config.SESSION_STRING
 
-# Hardcoded values
-userbot_version = "2.5.0"
-device_model = "YMusic-Userbot @Tech_Shreuansh29"
-system_version = f"{platform.system()} {platform.release()} {platform.machine()}"
-
 YMusicBot = Client(
-    name="YMusic",
-    api_id=api_id,
-    api_hash=api_hash,
-    session_string=session_string,
-    device_model=device_model,
-    system_version=system_version,
-    app_version=userbot_version,
-    workdir="./",
-    sleep_threshold=10,
-    parse_mode="html"
+    name="YMusic", api_id=api_id, api_hash=api_hash, session_string=session_string
 )
 
 YMusicUser = PyTgCalls(YMusicBot)
